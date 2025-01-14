985e7f556f4e5c2588d2328ab146f3076e03c8e6
billioniareman <thebillioniareman@gamil.com>
2025-01-15 01:30:35 +0530

    Prediction Pipeline and Flask work

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -3,26 +3,37 @@ import numpy as np
 from dataclasses import dataclass
 import os
 import sys
+
 from catboost import CatBoostRegressor
+
 from sklearn.ensemble import (
     AdaBoostRegressor,GradientBoostingRegressor,RandomForestRegressor
 )
+
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from  xgboost import XGBRegressor
+
+
 from src.exception import CustomException
 from src.logger import logging
 from src.utlis import save_object,evaluate_model
-@dataclass 
+
+
+
+@dataclass
 class ModelTrainerConfig:
     trained_model_file_path=os.path.join('artifact',"model.pkl")
+
+
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
+
     def initiate_model_trainer(self,train_array,test_array):
-        try:    
+        try:
             logging.info("Spliting Train and test data")
             X_train,y_train,X_test,y_test=(
                 train_array[:,:-1],
--- src/utlis.py
+++ src/utlis.py
@@ -1,6 +1,8 @@
 import os
 import pickle
 import sys
+
+from scipy.constants import elementary_charge
 from sklearn.metrics import r2_score
 from src.exception import CustomException
 
@@ -33,3 +35,10 @@ def evaluate_model(X_train,X_test,y_train,y_test,models):
 
     except Exception as e:
         CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise  CustomException(e,sys)
